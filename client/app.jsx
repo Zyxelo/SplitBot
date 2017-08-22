e78e512e15ca8ad2c0d7102989b47f53b9c0f9b6
Victor Ode <vicod194@student.liu.se>
2017-08-22 21:56:51 +0200

    testing materialui components

--- client/app.jsx
+++ client/app.jsx
@@ -18,18 +18,14 @@ import 'whatwg-fetch';
 
 /* ----------  External UI Kit  ---------- */
 
-import {
-  Button,
-  ButtonArea,
-  CellBody,
-  CellFooter,
-  CellHeader,
-  CellsTitle,
-  Form,
-  FormCell,
-  Slider,
-  Switch,
-} from 'react-weui';
+import Button from 'material-ui/Button';
+import Dialog, {
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+} from 'material-ui/Dialog';
+import Typography from 'material-ui/Typography';
 
 
 import userApi from '../messenger-api-helpers/user'
@@ -41,29 +37,51 @@ import {dateString} from '../utils/date-string-format';
 
 /* ----------  Models  ---------- */
 
+
 /* =============================================
    =            React Application              =
    ============================================= */
 
+const styles = {
+  container: {
+    textAlign: 'center',
+    paddingTop: 200,
+  },
+};
+
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
-      name: 'test'
+      name: 'test',
+      open: false,
+      tid: "demo"
     };
   }
 
+  handleRequestClose = () => {
+    this.setState({
+      open: false,
+    });
+  };
+
+  handleClick = () => {
+    this.setState({
+      open: true,
+    });
+  };
 
   componentWillMount() {
     if (this.props.userId !== 'demo') {
       userApi.getDetails(this.props.userId.psid ,(err, {statusCode}, body) => {
         console.log(body);
-        this.setState({name: body.first_name + body.last_name});
+        this.setState(
+          {name: body.first_name + body.last_name,
+            tid: this.props.userId.tid
+          }
+        );
       });
     }
-
-
   }
 
 
@@ -83,27 +101,27 @@ export default class App extends React.Component {
     /* ----------  Main Structure  ---------- */
 
     return (
-      <div className='app'>
-        <section>
-          <p>Hi {this.state.name}</p>
-          <h1>{JSON.stringify(this.props.userId)}</h1>
-          <CellsTitle>Date of Birth</CellsTitle>
-          <Form>
-            <FormCell select id='date-of-birth'>
-              <CellHeader id='display-date'>
-                {this.state.name}
-              </CellHeader>
-
-              <CellBody>
-                <input
-                  id='datepicker'
-                  type='date'
-                  required='required'
-                />
-              </CellBody>
-            </FormCell>
-          </Form>
-        </section>
+      <div className='app' style={styles.container}>
+        <Dialog open={this.state.open} onRequestClose={this.handleRequestClose}>
+          <DialogTitle>{this.state.tid}</DialogTitle>
+          <DialogContent>
+            <DialogContentText>1-2-3-4-5</DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button color="primary" onClick={this.handleRequestClose}>
+              OK
+            </Button>
+          </DialogActions>
+        </Dialog>
+        <Typography type="display1" gutterBottom>
+          Hi {this.state.name}
+        </Typography>
+        <Typography type="subheading" gutterBottom>
+          example subheading
+        </Typography>
+        <Button raised color="accent" onClick={this.handleClick}>
+          Whats my tid
+        </Button>
       </div>
     );
   }
